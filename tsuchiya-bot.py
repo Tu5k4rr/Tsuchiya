059a13b70f4deb54512637de59b101c8edd1062b
Tu5k4rr <21346295+Tu5k4rr@users.noreply.github.com>
2020-10-12 18:22:26 +0100

    Update tsuchiya-bot.py
    8ball command added

--- tsuchiya-bot.py
+++ tsuchiya-bot.py
@@ -117,6 +117,32 @@ async def ud (ctx):
     resid = random.randint(min, max)
     udurl = 'https://www.urbandictionary.com/random.php?page='
     await ctx.send(f'{udurl}{resid}')
+    
+
+@client.command()
+async def ball (ctx):
+    a= ["As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don’t count on it.",
+    "It is certain."
+    "It is decidedly so.",
+    "Most likely",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes – definitely.",
+    "You may rely on it."]
+    b = random.choice(a)
+    await ctx.send(f'{b}')
 
 
 client.run(token)

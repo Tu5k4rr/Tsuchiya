a51a3ddd4b9b5d40a7139d7c3069583976f00124
Tu5k4rr <21346295+Tu5k4rr@users.noreply.github.com>
2021-09-21 21:14:32 +0100

    Update gifs.py

--- commands/gifs.py
+++ commands/gifs.py
@@ -1,47 +1,40 @@
 from discord.ext import commands
 import discord, requests, json, random, os
+#urls
+#tenor
+tenor = 'https://api.tenor.com'
+#giphy
+giphy = 'https://api.giphy.com'
 
+#endpoint
+tran = '/v1/random?q=%s&key='
+gran = '/v1/gifs/random?api_key='
 
 #Setting up API key paths and files for gif services.
-home = os.path.expanduser("~")
-#Read in Giphy Key.
-with open(f'{home}/Tsuchiya/gifkey.txt', 'r') as gkey:
-        gtoken = gkey.read()
-#Read in Tenor Key.
-with open(f'{home}/Tsuchiya/keys/tenor.txt', 'r') as tg:
-        tkey = tg.read()
+cwd = os.getcwd()
+#tenor key
+with open(f'{cwd}/keys/tenor.txt', 'r') as tkey:
+    ttoken = tkey.read()
 
-#Happy Birthday
-def happycmd():
-    happyurl = (f'https://api.giphy.com/v1/gifs/random?api_key={gtoken}&tag=happybirthday')
-    reqhappy = requests.get(happyurl)
-    jsonhappy = json.loads(reqhappy.content)
-    reshappy = (jsonhappy['data']['embed_url'])
-    return reshappy
+#giphy key
+with open(f'{cwd}/keys/giphy.txt', 'r') as gkey:
+   gtoken = gkey.read()
 
-#Random Gif from Tenor or Giphy
-def rgifcmd():
-    turl = 'https://api.tenor.com/v1/random?q=%s&key='
-    treq = requests.get(f'{turl}{tkey}')
-    tjson = json.loads(treq.content)
-    tfinal = (tjson['results'][0]['url'])
-    gurl = 'https://api.giphy.com/v1/gifs/random?api_key='
-    bgurl = (f'{gurl}{gtoken}')
-    reqbgif = requests.get(bgurl)
-    bgjson = json.loads(reqbgif.content)
-    ffb = (bgjson['data']['embed_url'])
-    rr = random.choice([ffb, tfinal])
-    return rr
+def func_rtenor():
+    tt =  f'{tenor}{tran}{ttoken}'
+    t_req = requests.get(f'{tenor}{tran}{ttoken}')
+    tjson = json.loads(t_req.content)
+    tres = (tjson['results'][0]['url'])
+    return tres
 
-# Weeb Command for gifs matching "Weeb" - Request by Sanderpy.
-def weebcmd():
-    weeburl = 'https://api.tenor.com/v1/random?q=weeb&key='
-    weebreq = requests.get(f'{weeburl}{tkey}')
-    weebjson = json.loads(weebreq.content)
-    weebfinal = (weebjson['results'][0]['url'])
-    gweeburl = (f'https://api.giphy.com/v1/gifs/random?api_key={gtoken}&tag=goodnight')
-    weebgg = requests.get(gweeburl)
-    gweebjson = json.loads(weebgg.content)
-    gw = (gweebjson['data']['embed_url'])
-    wrr = random.choice([gw, weebfinal])
-    return wrr
+def func_rgiphy():
+    g_req = requests.get(f'{giphy}{gran}{gtoken}&tag=&rating=r')
+    gjson = json.loads(g_req.content)
+    gres = (gjson['data']['embed_url'])
+    return gres
+
+def_choice = [func_rgiphy, func_rtenor]
+
+def r_choice():
+    ret = random.choice(def_choice)()
+    return ret
